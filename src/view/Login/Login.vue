8725eaf070faa46dc6cc05fe8196c64b056900ef
闫韬蒙 <3094969938@qq.com>
2022-07-17 17:09:04 +0800

    perf: 删除了无效组件

--- src/view/Login/Login.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>login</div>
-</template>
-
-<script>
-export default {};
-</script>
-
-<style></style>
--- src/view/UserInfo/UserInfo.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>用户信息（需要登录）</div>
-</template>
-
-<script>
-export default {};
-</script>
-
-<style></style>
