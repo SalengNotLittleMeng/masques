0d48d8bbd052c14675fb62b7b45ebd8ff803c44b
闫韬蒙 <3094969938@qq.com>
2022-08-19 11:08:58 +0800

    docs: 修改了文档

--- src/descriptor/Api.js
+++ src/descriptor/Api.js
@@ -39,7 +39,7 @@ export const Api = function ({ module, url, params }) {
     descriptor.value = function (params, res, ...rest) {
       ajaxFun(params).then((_res) => {
         res = _res;
-        // 重新确定this指向
+        // 重新确定this指向并配置参数
         fn.call(this, params, res, ...rest);
       });
     };
