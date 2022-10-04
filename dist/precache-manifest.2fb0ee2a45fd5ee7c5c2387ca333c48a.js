06abef9c806b5d41ef622efcbb232581e71c3e97
闫韬蒙 <3094969938@qq.com>
2022-10-04 21:27:19 +0800

    ci: 更新了ci

---
+++ dist/precache-manifest.2fb0ee2a45fd5ee7c5c2387ca333c48a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: 'aa37f96e1a5ee31c1434911a2c8aaa44',
+    url: 'index.html',
+  },
+  {
+    revision: '34aaa59a33196650e1cc2b22894a99f3',
+    url: 'manifest.json',
+  },
+  {
+    revision: '735ab4f94fbcd57074377afca324c813',
+    url: 'robots.txt',
+  },
+  {
+    revision: 'ebb36a2bf6fae2fd4250',
+    url: 'static/css/app.902900de.css',
+  },
+  {
+    revision: '82b9c7a5a3f405032b1db71a25f67021',
+    url: 'static/img/logo.82b9c7a5.png',
+  },
+  {
+    revision: 'ebb36a2bf6fae2fd4250',
+    url: 'static/js/app.b771dd86.js',
+  },
+  {
+    revision: 'd44ff3056f8440a8121e',
+    url: 'static/js/chunk-vendors.1425716e.js',
+  },
+]);
