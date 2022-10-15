818d66d28067280813bb1a14eee0369f07aa4366
闫韬蒙 <3094969938@qq.com>
2022-10-15 20:38:03 +0800

    perf: 优化了clickoutside自定义指令

--- src/directive/clickoutside.js
+++ src/directive/clickoutside.js
@@ -1,16 +1,25 @@
-/** 点击添加自定义指令元素以外的地方之后触发事件
- * @param {Function} bind.value ：要触发的函数名,如果不填默认是使添加指令的元素消失
+/**点击添加自定义指令元素以外的地方之后触发事件
+ * @param {function} binding.value ：要触发的函数名,如果不填默认是使添加指令的元素消失
+ * @param {string} binding.arg ：相关联的类名，在关联类名的元素内点击不会触发消失事件,防止开启事件无法触发
  */
-// 调用方式
-// 点击其他区域后触发doPost事件
-/* <div class="test" v-clickoutside="doPost">hello </div> */
-// 点击其他区域后当前元素消失
-/* <div class="test" v-clickoutside>hello</div> */
+// 调用示例：
+//    <div class="add-item dir-user-defined" @click="addItem">自定义 ↓</div>
+//         <div class="to-select" v-show="showSelect"  v-clickoutside:dir-user-defined = "confirm"></div>
+
 export default {
-  beforeMount(el, binding) {
+  mounted(el, binding) {
+    const activePointClass = `.${binding.arg}`;
+    const activePoint = Array.from(document.querySelectorAll(activePointClass));
     document.addEventListener(
       'click',
       (e) => {
+        if (
+          activePoint.some((item) => {
+            return item.contains(e.target);
+          })
+        ) {
+          return;
+        }
         if (!el.contains(e.target)) {
           const func =
             binding.value ||
